903d8c9bf51fc6c6989d5799433bf2a078970cd3
Ziga Zajc <ziga.zajc007@gmail.com>
2024-09-01 14:25:18 +0200

    Add support for renameFile API endpoint

--- src/cloudky-api.ts
+++ src/cloudky-api.ts
@@ -230,6 +230,38 @@ namespace CloudkyAPI {
 			return Errors.getJson(Error.SERVER_UNREACHABLE);
 		}
 	}
+
+	export async function renameFile(server: string, username: string, token: string, path: string, destination: string): Promise<StandardResponse> {
+		if (!Validate.url(server)) return Errors.getJson(Error.SERVER_UNREACHABLE);
+		if (!Validate.username(username)) return Errors.getJson(Error.INVALID_USERNAME_FORMAT);
+		if (!Validate.token(token)) return Errors.getJson(Error.INVALID_TOKEN);
+		if (!Validate.userFilePathName(path)) return Errors.getJson(Error.INVALID_FILE_NAME);
+		if (!Validate.userFilePathName(destination)) return Errors.getJson(Error.INVALID_FILE_NAME);
+
+		try {
+			const data = {
+				path: path,
+				destination: destination,
+			};
+
+			const result = await fetch(server + "/v1/file/rename", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Basic ${btoa(username + ":" + token)}`,
+				},
+				body: JSON.stringify(data),
+			});
+
+			const response: StandardResponse = await result.json();
+			if (Validate.response(response)) return response;
+
+			return Errors.getJson(Error.UNKNOWN_ERROR);
+		} catch (err) {
+			if (err instanceof SyntaxError) return Errors.getJson(Error.INVALID_RESPONSE_FORMAT);
+			return Errors.getJson(Error.SERVER_UNREACHABLE);
+		}
+	}
 }
 
 export default CloudkyAPI;
---
+++ tests/validator/endpoints/file/file.rename.test.ts
@@ -0,0 +1,38 @@
+import { expect, test, describe } from "bun:test";
+import CloudkyAPI from "../../../../src/cloudky-api";
+import Blake2b from "@rabbit-company/blake2b";
+import type { StandardResponse } from "../../../../src/types";
+import { Error } from "../../../../src/errors";
+
+const server = "http://localhost:8085";
+const username = "test";
+const token = Blake2b.hash("P@ssword123");
+const file = "cats/cat.png";
+const destination = "cats/cat2.png";
+
+describe("file rename", () => {
+	test("invalid server", async () => {
+		const res: StandardResponse = await CloudkyAPI.renameFile("invalid server", username, token, file, destination);
+		expect(res.error).toBe(Error.SERVER_UNREACHABLE);
+	});
+
+	test("invalid username", async () => {
+		const res: StandardResponse = await CloudkyAPI.renameFile(server, "test.test123", token, file, destination);
+		expect(res.error).toBe(Error.INVALID_USERNAME_FORMAT);
+	});
+
+	test("invalid token", async () => {
+		const res: StandardResponse = await CloudkyAPI.renameFile(server, username, "test", file, destination);
+		expect(res.error).toBe(Error.INVALID_TOKEN);
+	});
+
+	test("invalid file name", async () => {
+		const res: StandardResponse = await CloudkyAPI.renameFile(server, username, token, "../test.png", destination);
+		expect(res.error).toBe(Error.INVALID_FILE_NAME);
+	});
+
+	test("invalid destination name", async () => {
+		const res: StandardResponse = await CloudkyAPI.renameFile(server, username, token, file, "../test");
+		expect(res.error).toBe(Error.INVALID_FILE_NAME);
+	});
+});
