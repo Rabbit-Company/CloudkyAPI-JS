af6bc264acac37aae39ef9a06622f06c1173740d
Ziga Zajc <ziga.zajc007@gmail.com>
2024-09-03 19:32:34 +0200

    Update

--- tests/validator/endpoints/sharelink/sharelink.create.test.ts
+++ tests/validator/endpoints/sharelink/sharelink.create.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from "bun:test";
-import { Blake2b, CloudkyAPI, Error, type StandardResponse } from "../../../../src/cloudky-api";
+import { Blake2b, CloudkyAPI, Error, type ShareLinkCreateResponse } from "../../../../src/cloudky-api";
 
 const server = "http://localhost:8085";
 const username = "test";
@@ -10,32 +10,32 @@ const expiration = Date.now() + 1_000_000_000;
 
 describe("sharelink create", () => {
 	test("invalid server", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink("invalid server", username, token, path, password, expiration);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink("invalid server", username, token, path, password, expiration);
 		expect(res.error).toBe(Error.SERVER_UNREACHABLE);
 	});
 
 	test("invalid username", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink(server, "test.test123", token, path, password, expiration);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink(server, "test.test123", token, path, password, expiration);
 		expect(res.error).toBe(Error.INVALID_USERNAME_FORMAT);
 	});
 
 	test("invalid token", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink(server, username, "test", path, password, expiration);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink(server, username, "test", path, password, expiration);
 		expect(res.error).toBe(Error.INVALID_TOKEN);
 	});
 
 	test("invalid file name", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink(server, username, token, "../test.png", password, expiration);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink(server, username, token, "../test.png", password, expiration);
 		expect(res.error).toBe(Error.INVALID_FILE_NAME);
 	});
 
 	test("invalid file password", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink(server, username, token, path, "Password123", expiration);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink(server, username, token, path, "Password123", expiration);
 		expect(res.error).toBe(Error.PASSWORD_TOO_WEAK);
 	});
 
 	test("invalid expiration", async () => {
-		const res: StandardResponse = await CloudkyAPI.createShareLink(server, username, token, path, password, 1000);
+		const res: ShareLinkCreateResponse = await CloudkyAPI.createShareLink(server, username, token, path, password, 1000);
 		expect(res.error).toBe(Error.INVALID_EXPIRATION_TIMESTAMP);
 	});
 });
